06ef0639384c1d30f2e1d4c9bb9b10ed9dc202cf
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-10-08 03:20:14 +0200

    fix user admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
+  ROLES = ['guest', 'user', 'manager', 'admin']
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
 
+  before_validation :set_role
+
+  def set_role
+    self[:role] = 'user' if self[:role].blank?
+  end
+
   def admin?
-    true # TODO - logic!
+    self[:role] == 'admin'
   end
 end
